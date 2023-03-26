7bc62cba5dd0aba7212273ec116eafc7fa9bd3e2
punsaraprathibha <punsaraprathibhagpp@gmail.com>
2023-03-26 17:01:42 +0530

    Comments added

--- src/lk/ijse/gdse/hibernate/layered/repository/CustomerRepository.java
+++ src/lk/ijse/gdse/hibernate/layered/repository/CustomerRepository.java
@@ -8,6 +8,9 @@ import java.util.List;
 
 public interface CustomerRepository extends CrudRepository<Customer, Long>{
 
+    // All the custom queries other than CRUD operation defined at CRUD Repository,
+    // you need for this repository should define here and implementation should
+    // define inside the implementation class
     List<Customer> getAllCustomers();
 
     List<Customer> getAllJPQLCustomers();
